53455f0e72df86c237bb6b31bce8b731ab6fdc5b
thatjuancodes <thejuan.codes@gmail.com>
2024-10-25 22:49:53 +0700

    define background color variable

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 const primaryColor = "#8A9A5B";
 const secondaryColor = "#D4AF37";
+const backgroundColor = "#FFC0CB";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
           <div className="bg-white bg-opacity-90 p-8 shadow-md rounded">
             <h1 style={{ color: primaryColor }} className="text-4xl">Welcome to Our Website</h1>
             <p style={{ color: primaryColor }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque leo nec magna fermentum, a tincidunt nisi facilisis. Integer sit amet arcu vel arcu vehicula fermentum.</p>
-            <button className="mt-4 px-4 py-2 bg-[#FFC0CB] rounded hover:border-[#D4AF37]" style={{ color: secondaryColor }}>Learn More</button>
+            <button className="mt-4 px-4 py-2 rounded hover:border-[#D4AF37]" style={{ backgroundColor, color: secondaryColor }}>Learn More</button>
           </div>
         </section>
 
@@ -64,7 +65,7 @@ export default function Home() {
         </section>
       </main>
 
-      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center bg-[#FFC0CB] p-4">
+      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center p-4" style={{ backgroundColor }}>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
